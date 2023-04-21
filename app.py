a2979f34b6a77157aedc9dedd7ec500779adeb52
chidananda4u <chidalegend@gmail.com>
2023-04-21 12:33:40 +0530

    created requirements.txt and get market summary

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+BITTREX_API_BASE_URL = "https://api.bittrex.com/v3"
+
 @app.route('/')
-def hello():
-    return ' Hello, World!'
+def get_market_summaries():
+    
+        response = requests.get(f"{BITTREX_API_BASE_URL}/markets/summaries")
+        return jsonify(response.json())
+   
+
 
 if __name__ == '__main__':
     app.run(debug=True)
