524fa34e6bddb348923e4e0c14882fdc5dba8444
chidananda4u <chidalegend@gmail.com>
2023-04-21 13:51:32 +0530

    adding jwt token

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 import requests
+from datetime import datetime, timedelta
+from functools import wraps
+from flask import  jsonify, request
 
+import jwt
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "dev"
+
 
 BITTREX_API_BASE_URL = "https://api.bittrex.com/v3"
+
+@auth.route("/generate_token")
+def generate_token():
+    exp_time = datetime.utcnow() + timedelta(seconds=1800)
+    token = jwt.encode({"user": "test"},app.config["SECRET_KEY"],algorithm="HS256",)
+    return jsonify({"token": token.decode("utf-8"), "expire": exp_time})
+
 
 @app.route("/market/summaries")
 def get_market_summaries():
