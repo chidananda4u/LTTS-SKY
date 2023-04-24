e6593abee6da0da365871d33e67603581c017d59
chidananda4u <chidalegend@gmail.com>
2023-04-24 05:30:21 +0530

    added uts with more coverage

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,30 +1,30 @@
+import os
+import sys
 import unittest
-from unittest.mock import patch
 from datetime import datetime, timedelta
+from unittest.mock import patch
+
 import jwt
 from flask import Flask
 
-import os
-import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.auth import auth
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'test_secret_key'
+app.config["SECRET_KEY"] = "test_secret_key"
 app.register_blueprint(auth)
 
 client = app.test_client()
 
-@patch('src.auth.datetime')
-@patch('src.auth.jwt')
+
+@patch("src.auth.datetime")
+@patch("src.auth.jwt")
 def test_generate_token(mock_jwt, mock_datetime):
+    """Unit test to test generate token"""
     mock_datetime.utcnow.return_value = datetime(2023, 4, 21, 12, 0, 0)
     exp_time = datetime.utcnow() + timedelta(seconds=1800)
-    mock_jwt.encode.return_value = b'test_token'
+    mock_jwt.encode.return_value = b"test_token"
 
-    response = client.get('/api/v1/auth/generate_token')
+    response = client.get("/api/v1/auth/generate_token")
     assert response.status_code == 200
-    assert response.json['token'] is not None
-
-    
+    assert response.json["token"] is not None
